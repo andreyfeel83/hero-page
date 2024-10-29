e4812caeafca8d9eb2d998b3471d765337e3b5ea
andreyfeel83 <andreyfeel83@gmail.com>
2024-10-29 17:36:57 +0200

    created hero page

---
+++ script.js
@@ -0,0 +1,20 @@
+const alldrinks = document.querySelectorAll('.drink');
+
+alldrinks.forEach((drink, index) => {
+	setTimeout(() => {
+		drink.style.display = 'block';
+	}, index * 1000);
+
+	drink.addEventListener('click', (e) => {
+		let activeDrink = e.target;
+
+		alldrinks.forEach((d) => {
+			if (d !== activeDrink) {
+				d.classList.remove('active');
+				d.style.display = 'none';
+			} else {
+				d.classList.add('active');
+			}
+		});
+	});
+});
